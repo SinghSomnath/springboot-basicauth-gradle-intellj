bbe92193a184d169fe05d85959048b8bbb4e4e47
somsingh1 <somnath.rcc@gmail.com>
2023-12-12 01:50:12 +0530

    Added @CrossOrigin(origins = "*") in the get calls

--- src/main/java/com/howtodoinjava/rest/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/rest/controller/EmployeeController.java
@@ -4,12 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.howtodoinjava.rest.dao.EmployeeDAO;
@@ -22,7 +17,8 @@ public class EmployeeController
 {
     @Autowired
     private EmployeeDAO employeeDao;
-    
+
+    @CrossOrigin(origins = "*")
     @GetMapping(path="/", produces = "application/json")
     public Employees getEmployees() 
     {
